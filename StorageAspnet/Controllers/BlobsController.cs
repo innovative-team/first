636923c718f0bd69b5e0c324c7f0aeeecaed29a6
Winnie <wwk773@gmail.com>
2019-01-21 08:53:22 +0630

    Add project files.

---
+++ StorageAspnet/Controllers/BlobsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StorageAspnet.Controllers
+{
+    public class BlobsController : Controller
+    {
+        // GET: Blobs
+        public ActionResult Index()
+        {
+            return View();
+        }
+        private CloudBlobContainer GetCloudBlobContainer()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                    CloudConfigurationManager.GetSetting("<wwk>_AzureStorageConnectionString"));
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("test-blob-container");
+            return container;
+        }
+        public string UploadBlob()
+        {
+            CloudBlobContainer container = GetCloudBlobContainer();
+            CloudBlockBlob blob = container.GetBlockBlobReference("myBlob");
+            using (var fileStream = System.IO.File.OpenRead(@"c:\src\sample.txt"))
+            {
+                blob.UploadFromStream(fileStream);
+            }
+            return "success!";
+        }
+    }
+}
---
+++ StorageAspnet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StorageAspnet.MvcApplication" Language="C#" %>
